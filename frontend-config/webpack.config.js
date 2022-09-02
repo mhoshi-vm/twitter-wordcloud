7248348b0c7550bced7e0ec12ab81114b9901452
machih <machih@vmware.com>
2022-09-02 11:25:29 +0900

    Fix nodejs detection

--- frontend-config/webpack.config.js
+++ frontend-config/webpack.config.js
@@ -1,18 +1,18 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/resources/static/notification.js',
+    entry: './notification.js',
     devtool: "inline-source-map",
     cache: true,
     mode: 'development',
     output: {
         path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js',
+        filename: '../src/main/resources/static/built/bundle.js',
     },
     module: {
         rules: [
             {
-                test: path.join(__dirname, '.'),
+                test: path.join(__dirname, './'),
                 exclude: /(node_modules)/,
                 use: [{
                     loader: 'babel-loader',
@@ -24,16 +24,7 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    publicPath: './built/',
-                    outputPath: './src/main/resources/static/built',
-                    filename: '[name][ext]'
-                },
-            },
+            }
         ]
     }
 };
