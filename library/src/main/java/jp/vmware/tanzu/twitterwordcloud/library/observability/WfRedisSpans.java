02293d3fe988c2c95d1590168fe72bb470a20282
machih <machih@vmware.com>
2022-11-14 13:21:15 +0900

    Add redis wavefront

--- library/src/main/java/jp/vmware/tanzu/twitterwordcloud/library/observability/WfRedisSpans.java
+++ library/src/main/java/jp/vmware/tanzu/twitterwordcloud/library/observability/WfRedisSpans.java
@@ -4,19 +4,15 @@ import brave.handler.MutableSpan;
 import brave.handler.SpanHandler;
 import brave.propagation.TraceContext;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WfRedisSpans {
 
-	private final Tracer tracer;
-
 	private final String appName;
 
-	public WfRedisSpans(Tracer tracer, @Value("${app.name}") String appName) {
-		this.tracer = tracer;
+	public WfRedisSpans(@Value("${app.name}") String appName) {
 		this.appName = appName;
 	}
 
